f2511b09d9ae4db5b92fda0357afd3a9016c8409
jfaquinojr <jfaquinojr@gmail.com>
2016-03-08 04:28:08 -0500

    Infrastructure

---
+++ Pochela-Suite/Pochela.Infrastructure/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pochela.Infrastructure
+{
+	public interface IQueryRepository<T> where T : BaseEntity
+	{
+		T GetById<TKey>(TKey Id);
+		IEnumerable<T> Search(Expression<Func<T, bool>> criteria);
+	}
+
+	public interface ICommandRepository<T> where T : BaseEntity
+	{
+		void Save(T model);
+		void Delete(T model);
+		void Create(T model);
+	}
+}
