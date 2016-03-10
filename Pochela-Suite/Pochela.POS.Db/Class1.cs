5ec97509cba2d54045b81df3dcb14113686235f2
jfaquinojr <jfaquinojr@gmail.com>
2016-03-09 19:39:18 -0500

    POS project structure

---
+++ Pochela-Suite/Pochela.POS.Db/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pochela.POS.Db
+{
+    public class Class1
+    {
+    }
+}
