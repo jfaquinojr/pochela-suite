af1996e2ea67ef1c7ba49653638903f4a142e18d
jfaquinojr <jfaquinojr@gmail.com>
2016-03-09 19:25:10 -0500

    move to folders

---
+++ Pochela-Suite/Inventory/Pochela.Inventory.Query/Mapper/ProductionSchemaMapper.cs
@@ -0,0 +1,17 @@
+﻿using DapperExtensions.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pochela.Inventory.Query.Mapper
+{
+	public class ProductionSchemaMapper<T> : AutoClassMapper<T> where T : class
+	{
+		public ProductionSchemaMapper() : base()
+		{
+			Schema("Production");
+		}
+	}
+}
