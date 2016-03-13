7ff4b6c6c5759a90874930c349ad12e6a0e59ce7
Jojo Aquino <jfaquinojr@gmail.com>
2016-03-13 13:59:38 +0800

    connectionfactgory

---
+++ Pochela-Suite/Infrastructure/Pochela.Infrastructure/IConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pochela.Infrastructure
+{
+	public interface IConnectionFactory
+	{
+		IDbConnection CreateConnection();
+	}
+
+
+	public class SqlConnectionFactory : IConnectionFactory
+	{
+		string _connectionString;
+
+		public SqlConnectionFactory(string connectionString)
+		{
+			_connectionString = connectionString;
+		}
+
+		public IDbConnection CreateConnection()
+		{
+			return new SqlConnection(_connectionString);
+		}
+	}
+}
